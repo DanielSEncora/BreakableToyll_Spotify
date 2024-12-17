b8da96c8bf8db6dac915f5ad282f7b2b1ead2409
Daniel Salas <daniel.salas@ENCEDCHMOLT0965.local>
2024-12-16 19:33:27 -0700

    Created project using spring.io

---
+++ BreakableToyll/src/test/java/encora/spark/BreakableToyll/SpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package encora.spark.BreakableToyll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
